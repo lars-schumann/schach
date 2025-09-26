f2195af88e4b5af76ebd3184bc3175f4b4e2a7dc
Lars Schumann <LarsSchumann@mail.de>
2025-09-26 18:53:41 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "rust dev shell for yew";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # Rust toolchain with the wasm32-unknown-unknown target
+          (rust-bin.stable.latest.default.override {
+            targets = [ "wasm32-unknown-unknown" ];
+          })
+          rust-analyzer
+          rustfmt
+
+          clippy
+          bacon
+
+          trunk
+
+          nixfmt
+          nixd
+        ];
+        env = {
+          RUST_BACKTRACE = "1";
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+
+        shellHook = ''
+          export PS1="\[\e[1;33m\]{chess}\[\e[0m\] $PS1"
+        '';
+      };
+    };
+}
