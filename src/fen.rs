4eb5b79515117f8ca61a86349ce0dd776f813ccb
Lars Schumann <LarsSchumann@mail.de>
2025-10-19 19:31:35 +0200

    full fen, still very ugly tho

--- src/fen.rs
+++ src/fen.rs
@@ -1,5 +1,6 @@
 use crate::{
     board::Board,
+    coord::{Col, Row, Square},
     game::{CastlingRights, GameState, Position},
     piece::Piece,
     player::PlayerKind,
@@ -10,7 +11,7 @@ struct FenStrings {
     piece_placements: Vec<AsciiChar>,
     active_player: String,
     castling_availability: String,
-    en_passant_target_square: Vec<AsciiChar>,
+    en_passant_target_square: String,
     half_move_clock: String,
     full_move_number: String,
 }
@@ -24,11 +25,11 @@ impl GameState {
             .collect::<Vec<String>>();
         let y: [String; 6] = x.try_into().expect("fen did not have all 6 fields");
 
-        let z: FenStrings = FenStrings {
+        let z = FenStrings {
             piece_placements: str_to_vec_ascii_char(&y[0]),
             active_player: y[1].clone(),
             castling_availability: y[2].clone(),
-            en_passant_target_square: str_to_vec_ascii_char(&y[3]),
+            en_passant_target_square: y[3].clone(),
             half_move_clock: y[4].clone(),
             full_move_number: y[5].clone(),
         };
@@ -68,6 +69,8 @@ impl GameState {
             _ => panic!(),
         };
 
+        let en_passant_target = str_to_square(&z.en_passant_target_square);
+
         Self {
             board,
             fifty_move_rule_clock,
@@ -76,7 +79,7 @@ impl GameState {
             position_history,
             is_perft: false,
             active_player,
-            en_passant_target: panic!(),
+            en_passant_target,
         }
     }
 }
@@ -122,4 +125,40 @@ fn fen_row_to_board_row(row: &[AsciiChar]) -> [Option<Piece>; 8] {
     out_row
         .try_into()
         .expect("why did the row not have 8 things in it :susge:")
+}
+
+fn str_to_square(str: &str) -> Option<Square> {
+    if str.contains('-') {
+        return None;
+    }
+
+    let square: [AsciiChar; 2] = str_to_vec_ascii_char(str)
+        .try_into()
+        .expect("square isnt 2 chars long hmm");
+
+    let col = match square[0] as u8 {
+        b'a' => Col::C1,
+        b'b' => Col::C2,
+        b'c' => Col::C3,
+        b'd' => Col::C4,
+        b'e' => Col::C5,
+        b'f' => Col::C6,
+        b'g' => Col::C7,
+        b'h' => Col::C8,
+        _ => panic!(),
+    };
+
+    let row = match square[1] as u8 {
+        b'1' => Row::R1,
+        b'2' => Row::R2,
+        b'3' => Row::R3,
+        b'4' => Row::R4,
+        b'5' => Row::R5,
+        b'6' => Row::R6,
+        b'7' => Row::R7,
+        b'8' => Row::R8,
+        _ => panic!(),
+    };
+
+    Some(Square { col, row })
 }
