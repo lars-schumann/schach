1dcac68a0b694a82e1c9d4ad0140d1c381978c82
Lars Schumann <LarsSchumann@mail.de>
2025-10-26 21:18:52 +0100

    unfuck the thing

--- src/tests.rs
+++ src/tests.rs
@@ -92,7 +92,7 @@ fn test_fens() {
 
 #[test]
 fn test_mass_fens() {
-    let fens = std::fs::read_to_string("../fens/lichess_puzzle_fens.txt").unwrap();
+    let fens = std::fs::read_to_string("./fens/lichess_puzzle_fens.txt").unwrap();
 
     for fen in fens.lines() {
         let schach_game = GameState::try_from_fen(fen).unwrap();
@@ -103,16 +103,17 @@ fn test_mass_fens() {
 
 #[test]
 fn test_mass_against_owl() {
-    let max_depth = 4;
+    let max_depth = 3;
     let max_fens = 10;
     let skip_fens = 100;
     let progress_thingy = std::cmp::max(max_fens / 1_000, 1);
-    let fens = std::fs::read_to_string("../fens/lichess_puzzle_fens.txt").unwrap();
+    let fens = std::fs::read_to_string("./fens/lichess_puzzle_fens.txt").unwrap();
 
     let mut progress = 0;
 
     for fen in fens.lines().skip(skip_fens).take(max_fens) {
-        let game = GameState::try_from_fen(fen).unwrap();
+        let mut game = GameState::try_from_fen(fen).unwrap();
+        game.is_perft = true;
         let _ = game.search(max_depth);
         progress += 1;
         if progress % progress_thingy == 0 {
