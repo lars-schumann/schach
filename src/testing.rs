d8853d790e11a166ceea29cd27d9f717a715ed52
Lars Schumann <LarsSchumann@mail.de>
2025-11-10 20:24:53 +0100

    put env var name in a macro

--- src/testing.rs
+++ src/testing.rs
@@ -1,14 +1,22 @@
+macro_rules! env_var {
+    () => {
+        "SCHACH_EXPENSIVE_TEST_OPT_IN"
+    };
+}
+
 macro_rules! skip_if_no_expensive_test_opt_in {
     () => {
-        if option_env!("SCHACH_EXPENSIVE_TEST_OPT_IN").is_none()
-            && std::env::var("SCHACH_EXPENSIVE_TEST_OPT_IN").is_err()
+        if option_env!(crate::testing::env_var!()).is_none()
+            && std::env::var(crate::testing::env_var!()).is_err()
         {
             println!(
-                "skipped because this is expensive, set env var SCHACH_EXPENSIVE_TEST_OPT_IN to run this"
+                "skipped because this is expensive, set env var {} to run this",
+                crate::testing::env_var!()
             );
             return;
         }
     };
 }
 
+pub(crate) use env_var;
 pub(crate) use skip_if_no_expensive_test_opt_in;
